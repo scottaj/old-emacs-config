2c2f59c5cfdeb405b937a61d01deeada0ce9c9a9
Al Scott <al.scott@atomicobject.com>
2013-01-29 00:29:26 -0500

    Added new init files

---
+++ init/init-rspec-mode.el
@@ -0,0 +1,5 @@
+(defadvice rspec-compile (around rspec-compile-around)
+  "Use BASH shell for running the specs because of ZSH issues."
+  (let ((shell-file-name "/bin/bash"))
+    ad-do-it))
+(ad-activate 'rspec-compile)
