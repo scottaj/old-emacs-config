9f7f5e6f55cd726df67f20d5b81ac5b6dcc896d1
Al Scott <al.scott@atomicobject.com>
2013-02-12 12:43:12 -0500

    Fix bug where path wasn't getting set right when running features.
    Bug seemed to be OSX specific, OSX like to occasionally reorder your path which then messes with rvm.
    Fixes #1

--- init/init-feature-mode.el
+++ init/init-feature-mode.el
@@ -1,1 +1,8 @@
+(defadvice feature-run-cucumber (around feature-run-cucumber-around)
+  "Use BASH shell for running the features because of ZSH issues."
+  (let ((shell-file-name "/bin/bash"))
+    ;; MAKE SURE RVM STUFF GETS STUCK ON THE FRONT OF THE PATH OR OSX WILL RUIN YOUR DAY
+    (setenv "PATH" (concat (first (rvm--emacs-ruby-binary)) ":" (getenv "PATH")))
+    ad-do-it))
+(ad-activate 'feature-run-cucumber)
 (setq feature-use-rvm t)
