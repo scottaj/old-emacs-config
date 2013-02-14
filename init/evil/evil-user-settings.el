e3b3c9b06b7472cf7aebc6e0a6a30d728cd713a9
Al Scott <al.scott@atomicobject.com>
2013-02-14 18:52:51 -0500

    Made it so cursor changes when switching to insert mode.

--- init/evil/evil-user-settings.el
+++ init/evil/evil-user-settings.el
@@ -1,8 +1,8 @@
 ;; Change cursor in different modes.
-;; (setq evil-default-cursor 'bar)
-;; (setq evil-normal-state-cursor 'box)
-;; (setq evil-visual-state-cursor 'box)
-;; (setq evil-replace-state-cursor 'box)
+(setq evil-default-cursor 'bar)
+(setq evil-normal-state-cursor 'box)
+(setq evil-visual-state-cursor 'box)
+(setq evil-replace-state-cursor 'box)
 
 ;; Set the initial evil state that certain major modes will be in.
 (evil-set-initial-state 'magit-log-edit-mode 'emacs)
